981767cf96b8b00c987eaef4149b1fb296d8ca65
Satish-Pradhan <154430887+Satish-Pradhan@users.noreply.github.com>
2023-12-28 06:07:14 +0000

    Datatypes concept

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -4,3 +4,27 @@
 
 console.log( 3 + 3 );
 console.log("Satish")
+
+
+let name = "Satish"
+let age = 21
+let isLoggedIn = true
+let temp = null
+let state ;
+
+
+//string => "preferable double qoutes"
+// number => 2 to power 53
+//bigint
+// boolean => true /false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// Object
+
+
+console.log(typeof "Satish");
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
