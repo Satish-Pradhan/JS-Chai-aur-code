ab602a7bc9e63a6ea1fbcd6b18ad4f3a2ce31c86
Satish-Pradhan <154430887+Satish-Pradhan@users.noreply.github.com>
2023-12-29 05:19:06 +0000

    Datatype Summary

---
+++ dataTypes_summary.js
@@ -0,0 +1,40 @@
+// Primitive Datatype
+/*7 types :
+ String, Number , Boolean , null ,undefined ,Symbol, BigInt
+ */
+const score = 100 //Number
+const scorevalue =100.3 //Number 
+
+const outsideTemp = null
+const isLoggedIn = false  // Boolean
+ // null
+let userEmail; //undefined
+let userEmail1= undefined // undefined
+
+const id = Symbol('123') //Symbol
+const anotherId = Symbol('123') // Symbol
+
+// console.log(id === anotherId); // false
+
+ const bigNumber = 34567392002863674n // BigInt
+
+ 
+ // Reference Datatype (Non primitive)
+ /*
+ Array, Object , Function
+ */
+
+ const heros = ["Shaktiman" , "Krish" ,"Ra-one", "FlyingJat"]; // Array
+  
+ let myobj = {        
+    name: "Satish",     // Object
+    age: 21,
+ }
+
+ const myFunction = function(){
+    console.log("Hello World");     //Function
+ }
+
+//All non primitive data type return type is Object.
+ 
+ console.log(typeof outsideTemp); // Object
