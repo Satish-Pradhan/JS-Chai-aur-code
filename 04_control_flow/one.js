c1babdd5e833b3c68b07b09033546497c501a0dc
Satish-Pradhan <154430887+Satish-Pradhan@users.noreply.github.com>
2024-01-03 06:38:04 +0000

    basics if control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,30 @@
+// if
+/*
+const isUserLoggedIn = true
+
+
+if(isUserLoggedIn){
+    console.log("executed");
+} else {
+    console.log("User logged in is false");
+} 
+
+// <, >, <=, >=, ==, !=, === , !==
+*/
+
+/*
+const score = 200
+
+if (score > 100){
+    let power = "fly"
+    console.log(`user power: ${power}`);
+}
+
+console.log(`user power : ${power}`); // error
+*/
+
+const balance = 1000
+
+//if ( balance > 500) console.log("test"), console.log("test2");
+
+
